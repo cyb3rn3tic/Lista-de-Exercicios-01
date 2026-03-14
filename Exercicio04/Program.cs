614a172cd90725ababaa47a8991a600ee469350b
Osmar Steffen <steffenosmar@gmail.com>
2026-03-14 17:03:57 -0300

    Exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+Console.Write("Digite a temperatura em Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius * 9/5) + 32;
+Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit}°F");
