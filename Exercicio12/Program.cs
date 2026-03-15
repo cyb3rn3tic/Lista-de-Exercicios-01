1c974fb7b57b0d36a1b0a33c60555b3a56b79a01
Osmar Steffen <steffenosmar@gmail.com>
2026-03-15 17:00:28 -0300

    Exercicio12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,21 @@
+﻿//12. Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de
+//impostos. Imprima o salário inicial, o salário com o aumento e o salário final.
+
+Console.Write("Digite o salário do funcionário: R$");
+double salarioInicial = Convert.ToDouble(Console.ReadLine());
+
+
+//double aumento = salarioInicial * 0.15; Da pra fazer assim também, mas é mais simples multiplicar direto por 1.15
+//double salarioComAumento = salarioInicial + aumento; 
+
+double salarioComAumento = salarioInicial * 1.15;
+
+//double imposto = salarioComAumento * 0.08; Da pra fazer assim também, mas é mais simples multiplicar direto por 0.92 (100% - 8% = 92% = 0.92)
+//double salarioFinal = salarioComAumento - imposto;
+
+double salarioFinal = salarioComAumento * 0.92;
+
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine($"Salário inicial: R${salarioInicial:F2}");
+Console.WriteLine($"Salário com aumento de 15%: R${salarioComAumento:F2}");
+Console.WriteLine($"Salário final após desconto de 8% de impostos: R${salarioFinal:F2}");
