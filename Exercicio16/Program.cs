90937dd182c3b1985c99d6b2979184dbb0f253ad
Osmar Steffen <steffenosmar@gmail.com>
2026-03-15 17:34:14 -0300

    Exercicio16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,16 @@
+﻿// 16. Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+Console.Write("Digite um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("-------------------------");
+    Console.WriteLine($"O número {numero} é par.");
+}
+
+else
+{
+    Console.WriteLine("---------------------------");
+    Console.WriteLine($"O número {numero} é ímpar.");
+}
