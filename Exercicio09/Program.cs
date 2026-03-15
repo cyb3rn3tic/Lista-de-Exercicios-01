4afad8306ce8efe3ed206706da938d071c0ca744
Osmar Steffen <steffenosmar@gmail.com>
2026-03-15 11:48:56 -0300

    Exercicio09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,13 @@
+﻿// 9. A imobiliária Imóbilis vende apenas terrenos retangulares. 
+// Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno.
+
+Console.Write("Digite a largura do terreno em metros: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o comprimento do terreno em metros: ");
+double comprimento = Convert.ToDouble(Console.ReadLine());
+
+double area = largura * comprimento;
+
+Console.WriteLine($"--------------------------------------------------");
+Console.WriteLine($"A área do terreno é: {area} metros quadrados.");
