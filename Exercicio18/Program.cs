149e2a79e116f60efd5dc5c50a22df0f13d35a95
Osmar Steffen <steffenosmar@gmail.com>
2026-03-15 19:55:10 -0300

    Exercicio18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,8 @@
+﻿// 18. Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+
+Console.WriteLine("Números ímpares entre 100 e 200:");
+
+for (int i = 101; i < 200; i += 2) // como é um intervalo que da pra saber quais os impares, podemos iniciar a variável com o primeiro número ímpar e ir somando 2 para obter os próximos ímpares
+{
+    Console.Write($"{i}, ");
+}
